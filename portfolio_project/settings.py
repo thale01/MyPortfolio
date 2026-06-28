f63f0274dc6e6367cc753a47d904699cf263f55b
thale01 <siddhithale01@gmail.com>
2026-06-28 12:24:15 +0530

    Update STATIC_URL and MEDIA_URL to absolute paths with leading slash

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -118,11 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files for uploaded project screenshots
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
